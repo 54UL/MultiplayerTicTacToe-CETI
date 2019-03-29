7bbc0df42bc3ae8db3bcce80cbb9d1edf922f63f
XV02 <lghhssi@gmail.com>
2019-03-29 14:32:44 -0600

    Jajaja Nunca hice pull

--- Muestra/tictactoeProyecto/tictactoeProyecto/_GTO.cpp
+++ Muestra/tictactoeProyecto/tictactoeProyecto/_GTO.cpp
@@ -4,9 +4,10 @@ using namespace System::Windows::Forms;
 using namespace System;
 
 [STAThread]
-void main(array<String^> ^ argv) 
+int main(array<String^> ^ argv) 
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::Run(gcnew tictactoeProyecto::_GTO());
+	return 0;
 }
